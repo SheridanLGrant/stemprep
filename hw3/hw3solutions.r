3fef30af13bf8383b1cf9e46b02312cf7eb9f991
Sheridan Grant <sheridanlgrant@gmail.com>
2017-07-13 15:24:15 -0700

    solutions

---
+++ hw3/hw3solutions.r
@@ -0,0 +1,19 @@
+# 1
+sum(sqrt(1:10000))
+
+# 2
+min(tan(100:200))
+
+# 3
+1500
+
+# 4
+firstFive <- function(x) {
+  if (length(x) < 5) {
+    print("Oops! Vector too short.")
+  } else {
+    return(x[1:5])
+  }
+}
+
+# 5
