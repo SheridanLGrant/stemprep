86bd1bb6466a267aeb54b5fc72b30cb48b6f236c
Sheridan Grant <sheridanlgrant@gmail.com>
2017-07-14 16:34:54 -0700

    finished solutions

--- hw3/hw3solutions.r
+++ hw3/hw3solutions.r
@@ -5,9 +5,14 @@ sum(sqrt(1:10000))
 min(tan(100:200))
 
 # 3
-1500
+mean(seq(1001,1999,2))
 
 # 4
+f <- function(x) {
+  return(4*x^2 + 3*x + sqrt(x) + sin(x) + 7)
+}
+
+# 5
 firstFive <- function(x) {
   if (length(x) < 5) {
     print("Oops! Vector too short.")
@@ -16,4 +21,18 @@ firstFive <- function(x) {
   }
 }
 
-# 5
+# 6
+sumOdd <- function(n) {
+  return(sum(seq(1,n,2)))
+}
+
+# 7
+middle <- function(a,b,c) {
+  if (((a <= b) & (a >= c)) | ((a <= c) & (a >= b))) {
+    return(a)
+  } else if (((b <= a) & (b >= c)) | ((b <= c) & (b >= a))) {
+    return(b)
+  } else {
+    return(c)
+  }
+}
