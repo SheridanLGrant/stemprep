3bad649ef4a315a86893cbdc83467bb315b5b2c0
Sheridan Grant <grantsl@nih.gov>
2017-07-31 15:20:56 -0700

    forgot to save ugh

--- hw5/hw5solutions.r
+++ hw5/hw5solutions.r
@@ -5,8 +5,21 @@ d$height <- d$Height..ft. + d$Height..in./12
 boxplot(d$height ~ d$Shoe.Gender)
 
 # 3
+
 # Count the number of women in the class
 women <- length(d$height[d$Shoe.Gender == 'w'])
-# Count the number of women who are shorter than 5'4"
+
+# Count the number of women who are as short or shorter than 5'4"
 # and divide by the number of women
 length(d$height[d$height <= 5 + 1/3 & d$Shoe.Gender == 'w']) / women
+
+# Same but taller than 5'8"
+length(d$height[d$height > 5 + 2/3 & d$Shoe.Gender == 'w']) / women
+
+# How many girls 5'5" or taller?
+tallGirls <- length(d$height[d$height >= 5 + 5/12 & d$Shoe.Gender == 'w'])
+# Only one Idara, so the probability of choosing Idara given that he's picking
+# a tall girl is
+1 / tallGirls
+
+length(d$height[d$height == 5 & d$Shoe.Gender == 'w' & d$State == 'HI']) / women
