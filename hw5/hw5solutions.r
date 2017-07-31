cbc9297fdb8a461f4f043320297911704e4bf873
Sheridan Grant <sheridanlgrant@gmail.com>
2017-07-31 15:15:54 -0700

    finished writing solutions

--- hw5/hw5solutions.r
+++ hw5/hw5solutions.r
@@ -10,3 +10,11 @@ women <- length(d$height[d$Shoe.Gender == 'w'])
 # Count the number of women who are shorter than 5'4"
 # and divide by the number of women
 length(d$height[d$height <= 5 + 1/3 & d$Shoe.Gender == 'w']) / women
+
+# 4
+quantile(d$height[d$Shoe.Gender == 'm'], .75) # 3rd quartile of the boys
+max(d$height[d$Shoe.Gender == 'w']) # Idara
+
+# Would Pearson be an outlier?
+quantile(d$height[d$Shoe.Gender == 'w'], .75) + 
+  (quantile(d$height[d$Shoe.Gender == 'w'], .75) - quantile(d$height[d$Shoe.Gender == 'w'], .25))*1.5
