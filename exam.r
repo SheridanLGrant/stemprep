efc13b3c8fe8c75913c86f677c2431726455af2d
Sheridan Grant <sheridanlgrant@gmail.com>
2017-09-12 18:55:10 -0700

    organized this

--- exam.r
+++
@@ -1,4 +0,0 @@
-a <- rnorm(15,5,2)
-b <- rnorm(25,1,4)
-boxplot(a,b, main = 'Warp Times for Different Spaceships', 
-        xlab = 'Spaceship Number', ylab = 'Warp Time (ms)')
---
+++ exam/exam.r
@@ -0,0 +1,7 @@
+a <- rnorm(15,5,2)
+b <- rnorm(25,1,4)
+boxplot(a,b, main = 'Warp Times for Different Spaceships', 
+        xlab = 'Spaceship', ylab = 'Warp Time (ms)', names = c('Xenith', 'Forsyth'))
+
+mean((1:100)^3)
+sd(c(2,7,1,8,2,8,1,8,2))
